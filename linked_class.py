722bc3374d6e3b31aea00842a0345632d9c11ebb
Akmaral <47251442+Akmaralka@users.noreply.github.com>
2019-04-25 13:07:13 +0600

    Update linked_class.py

--- linked_class.py
+++ linked_class.py
@@ -1,47 +1,60 @@
 from myiterator import MyIterator
 
-
-class FDS:
-    def __init__(self):
-        self.array = []
-
-    def enqueue(self, item):
-        self.array.insert(0, item)
-
-    def dequeue(self):
-        if self.is_empty():
-            return None
-        else:
-            return self.array.pop()
-
-    def pop(self):
-        if self.is_empty():
-            return None
-        return self.array.pop()
-
-    def push(self, item):
-        return self.array.append(item)
-
-    def peak(self):
-        if self.is_empty():
-            return None
-        else:
-            return self.array[-1]
-
-    def size(self):
-        return len(self.array)
-
-    def is_empty(self):
-        return self.size() == 0
+class Set:
+    def __init__(self, elements=None):
+        self.elements = {}
+        if elements != None:
+            if len(elements) != len(set(elements)):
+                elements = set(elements)
+            for element in elements:
+                self.elements[element] = element
 
     def __iter__(self):
-        return MyIterator(self.array)
+        return MyIterator(self.elements)
 
+    def add(self, element):
+        if element in self.elements.keys():
+            return 'The element {0} has already been in the set'.format(element)
+        self.elements[element] = element
 
-class ListNode:
-    def __init__(self, data=None):
-        self.data = data
-        self.next = None
+    def remove(self, element):
+        if element not in self.elements.keys():
+            return 'The element {0} not in the set'.format(element)
+        self.elements.pop(element)
+
+    def size(self):
+        return len(self.elements.keys())
+
+    def contains(self, element):
+        if element in self.elements.keys():
+            return True
+        return False
+
+    def equals(self, otherSet):
+        if self.elements.keys() == otherSet.elements.keys():
+            return True
+        else:
+            return False
+
+    def union(self, otherSet):
+        newSet = list(set(self.elements.keys()) | set(otherSet.elements.keys()))
+        unionSet = Set(newSet)
+        return unionSet
+
+    def intersection(self, otherSet):
+        newSet = list(set(self.elements.keys()) & set(otherSet.elements.keys()))
+        intersectionSet = Set(newSet)
+        return intersectionSet
+
+    def difference(self, otherSet):
+        newSet = list(set(self.elements.keys()) ^ set(otherSet.elements.keys()))
+        differenceSet = Set(newSet)
+        return differenceSet
+
+    def is_subset_of(self, otherSet):
+        if set(self.elements.keys()) < set(otherSet.elements.keys()):
+            return True
+        return False
 
     def __str__(self):
-        return str(self.data)
+        return '[' + ', '.join(str(x) for x in self.elements.keys()) + ']'
